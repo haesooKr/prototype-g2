a4914afeaa00324d8051d1d9a3aed68f548bc1d4
haesooKr <59234874+haesooKr@users.noreply.github.com>
2020-02-17 17:01:41 -0500

    Add PlayerController script

---
+++ Assets/Scenes/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertial = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertial);
+
+        rb.AddForce(movement * speed);
+    }
+}
