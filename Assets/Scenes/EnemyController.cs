9ecb57e56c851073a5cd4dba80d79d67a69d10e8
haesooKr <59234874+haesooKr@users.noreply.github.com>
2020-02-17 17:19:39 -0500

    Add EnemyController
    automatically follows player

---
+++ Assets/Scenes/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform Player;
+    public int MoveSpeed = 1;
+    public int MinDist = 0;
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if(Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        }
+    }
+}
