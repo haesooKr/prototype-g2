190adfd5a2df6d1e829cabdb56c12e0bc2d2ca7c
haesooKr <59234874+haesooKr@users.noreply.github.com>
2020-02-17 17:57:39 -0500

    Add Timer Script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float timeRecord;
+    void Update()
+    {
+        timeRecord += Time.deltaTime;
+
+        float minutes = timeRecord / 60;
+        float seconds = timeRecord % 60;
+
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        
+    }
+}
